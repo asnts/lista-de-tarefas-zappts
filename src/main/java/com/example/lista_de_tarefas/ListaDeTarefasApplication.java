3b61387c422d26d119114358589013df9ad1b04f
asnts <annandasants@gmail.com>
2024-11-27 17:34:45 -0300

    feat: ajuste na main

--- src/main/java/com/example/lista_de_tarefas/ListaDeTarefasApplication.java
+++ src/main/java/com/example/lista_de_tarefas/ListaDeTarefasApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ListaDeTarefasApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ListaDeTarefasApplication.class, args);
 	}
 

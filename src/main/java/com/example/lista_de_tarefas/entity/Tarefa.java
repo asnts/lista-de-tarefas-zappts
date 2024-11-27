80283374aa5f585126b54bd084b7b84c880d55a6
asnts <annandasants@gmail.com>
2024-11-27 17:36:41 -0300

    feat: inserção de @notblank e getters/setters

--- src/main/java/com/example/lista_de_tarefas/entity/Tarefa.java
+++ src/main/java/com/example/lista_de_tarefas/entity/Tarefa.java
@@ -1,8 +1,7 @@
 package com.example.lista_de_tarefas.entity;
-import jakarta.persistence.*;
 
-import java.time.Instant;
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
@@ -11,9 +10,12 @@ public class Tarefa {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank
     @Column(nullable = false)
     private String titulo;
-    private  String descricao;
+
+    @NotBlank
+    private String descricao;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -21,13 +23,70 @@ public class Tarefa {
 
     @Column(nullable = false, updatable = false)
     private LocalDateTime dataCriacao;
+
     private LocalDateTime dataConclusao;
 
+    public Tarefa() {
+        // Construtor padrão
+    }
+
+    public Tarefa(String titulo, String descricao, Status status) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDateTime dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
     @PrePersist
-
-    protected void onCreate(){
-        Instant localDateTime = null;
-        this.dataCriacao = LocalDateTime.from(localDateTime.now());
-
+    protected void onCreate() {
+        this.dataCriacao = LocalDateTime.now();
     }
 }

c777fd297dbc01a52f92a4f26447a34a28792b4e
asnts <annandasants@gmail.com>
2024-11-27 20:17:17 -0300

    feat: atualização de imports

--- src/main/java/com/example/lista_de_tarefas/repository/TarefaRepository.java
+++ src/main/java/com/example/lista_de_tarefas/repository/TarefaRepository.java
@@ -1,7 +1,8 @@
 package com.example.lista_de_tarefas.repository;
-
 import com.example.lista_de_tarefas.entity.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }

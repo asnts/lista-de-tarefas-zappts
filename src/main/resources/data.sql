089e96005543b5990b695833d3bdf07fee2341d8
asnts <annandasants@gmail.com>
2024-11-28 20:50:20 -0300

    feat: add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tarefas (titulo, descricao, status, dataConclusao)
+VALUES
+('Tarefa 1', 'Descrição da tarefa 1', 'PENDENTE', NULL),
+('Tarefa 2', 'Descrição da tarefa 2', 'EM_ANDAMENTO', NULL),
+('Tarefa 3', 'Descrição da tarefa 3', 'CONCLUIDA', '2024-11-01 10:30:00');

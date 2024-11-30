f06c3736f0dc9deb62cf0f05bed2c8b7ca574832
asnts <annandasants@gmail.com>
2024-11-30 19:33:41 -0300

    feat: add TarefaServiceTest.java

---
+++ src/test/java/com/example/lista_de_tarefas/TarefaServiceTest.java
@@ -0,0 +1,106 @@
+package com.example.lista_de_tarefas;
+
+import com.example.lista_de_tarefas.entity.Status;
+import com.example.lista_de_tarefas.entity.Tarefa;
+import com.example.lista_de_tarefas.repository.TarefaRepository;
+import com.example.lista_de_tarefas.service.TarefaService;
+import org.apache.coyote.BadRequestException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class  TarefaServiceTest{
+
+    @Mock
+
+    private TarefaRepository tarefaRepository;
+
+    @InjectMocks
+
+    private TarefaService tarefaService;
+
+    public  TarefaServiceTest(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void criarTarefaComSucesso(){
+
+        Tarefa novaTarefa = new Tarefa();
+        novaTarefa.setTitulo("Teste");
+        novaTarefa.setDescricao("Descrição de teste");
+
+                when(tarefaRepository.save(any(Tarefa.class))).thenReturn(novaTarefa);
+
+                Tarefa tarefaCriada = tarefaService.criarTarefa(novaTarefa);
+
+                assertNotNull(tarefaCriada);
+                assertEquals("Teste", tarefaCriada.getTitulo());
+                verify(tarefaRepository, times(1)).save(novaTarefa);
+
+    }
+
+    private void assertNotNull(Tarefa tarefaCriada) {
+    }
+
+
+    @Test
+
+    void atualizarTarefaComSucesso() throws BadRequestException {
+
+        Long tarefaId = 1L;
+
+        Tarefa tarefaExistente = new Tarefa();
+
+        tarefaExistente.setId(tarefaId);
+        tarefaExistente.setTitulo("Titulo Antigo");
+        tarefaExistente.setDescricao("Descricao Antiga");
+        tarefaExistente.setStatus(Status.PENDENTE);
+
+
+        Tarefa tarefaAtualizada = new Tarefa();
+
+        tarefaAtualizada.setTitulo("Novo Titulo");
+        tarefaAtualizada.setDescricao("Nova Descricao");
+        tarefaAtualizada.setStatus(Status.CONCLUIDA);
+
+        when(tarefaRepository.findById(tarefaId)).thenReturn(java.util.Optional.of(tarefaExistente));
+        when(tarefaRepository.save(any(Tarefa.class))).thenReturn(tarefaExistente);
+
+        Tarefa resultado = tarefaService.atualizarTarefa(tarefaId, tarefaAtualizada);
+
+        assertNotNull(resultado);
+        assertEquals("Novo titulo", resultado.getTitulo());
+        assertEquals("Nova Descrição", resultado.getDescricao());
+        assertEquals(Status.CONCLUIDA, resultado.getStatus());
+        assertNotNull(resultado.getDataConclusao());
+        verify(tarefaRepository, times(1)).findById(tarefaId);
+        verify(tarefaRepository, times(1)).save(tarefaExistente);
+
+
+    }
+
+
+    @Test
+
+    void lancarExcecaoSeTarefaNaoForEncontrada(){
+
+        Long tarefaId = 1L;
+
+        when(tarefaRepository.findById(tarefaId)).thenReturn(java.util.Optional.empty());
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () ->
+             tarefaService.atualizarTarefa(tarefaId, new Tarefa()));
+
+
+        assertEquals("Tarefa com ID" + tarefaId + "não encontrada", exception.getCause().getMessage());
+        verify(tarefaRepository, times(1)).findById(tarefaId);
+
+    }
+}
